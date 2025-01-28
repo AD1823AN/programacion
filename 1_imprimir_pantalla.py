e2e2b498d8c1f6ef9ca58902328b4cb9d8bd074d
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-01-28 17:54:30 -0600

    Update 1_imprimir_pantalla.py

--- 1_imprimir_pantalla.py
+++ 1_imprimir_pantalla.py
@@ -1,3 +1,5 @@
 #El hola mundo es la frace con la que nos referimos para poder expresar que nuestro programa esta 
 #funcional 
+#sirve como punto de partida para que los programadores exploren las características
+#y la sintaxis de un nuevo lenguaje de programación
 print("Hola Adri, bienvenida a tu primer !Hola mundo! en python")
