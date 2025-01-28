e6f2aeb07b31b39f1ec4b57f083332ee3a3fc056
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-01-27 21:37:04 -0600

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,3 +1,4 @@
-numeros = [int(x) for x in input("Ingrese una lista de números separados por espacios: ").split()]
+numeros = [int(x) for x in input("Ingrese una lista de números separados por 
+espacios: ").split()]
 cantidad_pares = sum(1 for numero in numeros if numero % 2 == 0)
 print("Cantidad de números pares:", cantidad_pares)
