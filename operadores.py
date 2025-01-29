fd6f0d2a6fb189c04fc1dab728426a90b954e325
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-01-28 19:21:30 -0600

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,15 @@
+# Declaración de variables
+nombre = "Adriana"
+edad = 21
+altura = 1.62
+ 
+# Operaciones
+suma = 5 + 3
+resta = 10 - 2
+multiplicacion = 4 * 6
+division = 15 / 3
+modulo = 17 % 4  # Resto de la división
+operacion = "la operacion es"
+## Concatenación de cadenas
+saludo = "Hola, " + nombre + ". Tu edad es: " + str(edad) + ". Tu altura es: " + str(altura) + ". " + operacion + str(suma)
+print(saludo)
