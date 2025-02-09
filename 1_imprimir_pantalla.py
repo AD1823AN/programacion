85297fc0c564ad9417bc12d05261e0fd2c99b216
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-02-08 23:59:22 -0600

    Actualizar 1_imprimir_pantalla.py

--- 1_imprimir_pantalla.py
+++ 1_imprimir_pantalla.py
@@ -1,4 +1,4 @@
-#El hola mundo es la frace con la que nos referimos para poder expresar que nuestro programa esta 
+ #El hola mundo es la frace con la que nos referimos para poder expresar que nuestro programa esta 
 #funcional 
 #sirve como punto de partida para que los programadores exploren las características
 #y la sintaxis de un nuevo lenguaje de programación
