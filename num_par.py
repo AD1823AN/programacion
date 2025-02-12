565d377b92821034ce04d7110e764080ec065d03
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-02-12 12:48:05 -0600

    Update num_par.py

--- num_par.py
+++ num_par.py
@@ -9,6 +9,7 @@ def contar_pares(lista):
             contador += 1
             
     return contador
+    
 entrada = input("Ingrese una lista de números separados por espacios: ")
 numeros = [int(x) for x in entrada.split()]
 cantidad_pares = contar_pares(numeros)
